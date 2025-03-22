13a3b70a122baccd8ba86d096b8e1f91e7be64c4
1078971-ibrahim <1078971@hr.nl>
2025-03-22 02:50:42 +0100

    feat: flipperstructuur

---
+++ flipperkast/game/flipper.py
@@ -0,0 +1,9 @@
+class Flipper:
+    def __init__(self, side: str):
+        self.side = side
+        self.angle = 0
+        self.active = False
+
+    def activate(self):
+
+    def deactivate(self):
