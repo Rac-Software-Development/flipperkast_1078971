b3c134b25dc41af17227fd74a9aa02d66c68360f
1078971-ibrahim <1078971@hr.nl>
2025-03-22 02:46:04 +0100

    Feat: class Ball

---
+++ flipperkast/game/ball.py
@@ -0,0 +1,11 @@
+class Ball:
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+        self.velocity_x = 0
+        self.velocity_y = 0
+
+    def move(self):
+
+    def check_collision(self, obj):
+        
