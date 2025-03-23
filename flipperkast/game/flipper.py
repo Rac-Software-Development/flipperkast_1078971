1cf24482d020b6dfbdc567786057f092cc63569b
1078971-ibrahim <1078971@hr.nl>
2025-03-23 02:08:55 +0100

    #2 input & hitball implemented

--- flipperkast/game/flipper.py
+++ flipperkast/game/flipper.py
@@ -5,7 +5,17 @@ class Flipper:
         self.active = False
 
     def activate(self):
-        pass
+        self.active = True
+        self.angle = 30 if self.side == 'left' else -30  # voorbeeldhoek
 
     def deactivate(self):
-        pass
+        self.active = False
+        self.angle = 0
+
+    def hit_ball(self, ball):
+        if self.active:
+            ball.velocity_y = -abs(ball.velocity_y)
+            if self.side == 'left':
+                ball.velocity_x -= 2
+            else:
+                ball.velocity_x += 2
