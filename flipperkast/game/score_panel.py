1e2a3d42235745aa224bea64dcd9acab2b7f5059
1078971-ibrahim <1078971@hr.nl>
2025-03-23 01:34:00 +0100

    feat: class scorepanel

---
+++ flipperkast/game/score_panel.py
@@ -0,0 +1,8 @@
+class ScorePanel:
+    def __init__(self):
+        self.scores = {}
+        self.highscore = 0
+
+    def update_score(self, player_id: str, points: int):
+        # TODO: Update score voor specifieke speler
+        pass
