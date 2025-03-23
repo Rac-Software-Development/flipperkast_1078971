ed19fdc7a33264e587c0e8d358fbe683afd7adbe
1078971-ibrahim <1078971@hr.nl>
2025-03-23 01:47:20 +0100

    feat: #1 move function

--- flipperkast/game/ball.py
+++ flipperkast/game/ball.py
@@ -2,10 +2,12 @@ class Ball:
     def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
-        self.velocity_x = 0
-        self.velocity_y = 0
+        self.velocity_x = 2
+        self.velocity_y = 3
 
     def move(self):
-        pass
+        self.x += self.velocity_x
+        self.y += self.velocity_y
+
     def check_collision(self, obj):
         pass
