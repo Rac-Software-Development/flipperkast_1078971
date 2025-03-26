07cff0ad66a7a9fff7d9f1d4708f9e86e3bdf9ef
1078971-ibrahim <1078971@hr.nl>
2025-03-26 21:34:51 +0100

    #5 Feat: Scoreboard added + MQTT + Highscore

--- flipperkast/game/score_panel.py
+++ flipperkast/game/score_panel.py
@@ -1,8 +1,19 @@
+from mqtt.mqtt_client import MQTTClient
+from mqtt.topics import SCORE_TOPIC
 class ScorePanel:
     def __init__(self):
-        self.scores = {}
+        self.scores = {"Player": 0}
         self.highscore = 0
+        self.mqtt = MQTTClient()
+        self.mqtt.client.on_message = self.on_message
+        self.mqtt.client.subscribe(SCORE_TOPIC)
+        self.mqtt.start()
 
-    def update_score(self, player_id: str, points: int):
-        # TODO: Update score voor specifieke speler
-        pass
+    def on_message(self, client, userdata, msg):
+        score = int(msg.payload.decode())
+        self.scores["Player"] += score
+
+        if self.scores["Player"] > self.highscore:
+            self.highscore = self.scores["Player"]
+
+        print(f"Score: {self.scores['Player']} |Highscore: {self.highscore}")
