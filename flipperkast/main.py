09504f01d659e683520d743558ff2ff53f5e57e9
1078971-ibrahim <1078971@hr.nl>
2025-03-26 21:35:48 +0100

    #5 scorepanel bond

--- flipperkast/main.py
+++ flipperkast/main.py
@@ -1,5 +1,8 @@
 from game.game_manager import GameManager
+from game.score_panel import ScorePanel
 
 if __name__ == "__main__":
+    scorepanel = ScorePanel() 
     game = GameManager()
     game.run()
+    scorepanel.mqtt.stop() 
