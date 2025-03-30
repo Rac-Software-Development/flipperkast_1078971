aaaf29b449ef99ad2ba4e6104fb3f769b3b5acb4
1078971-ibrahim <1078971@hr.nl>
2025-03-30 15:34:54 +0200

    added highscore

--- flipperkast/game/score_panel.py
+++ flipperkast/game/score_panel.py
@@ -1,5 +1,6 @@
 from mqtt.mqtt_client import MQTTClient
 from mqtt.topics import SCORE_TOPIC
+
 class ScorePanel:
     def __init__(self):
         self.scores = {"Player": 0}
@@ -8,12 +9,12 @@ class ScorePanel:
         self.mqtt.client.on_message = self.on_message
         self.mqtt.client.subscribe(SCORE_TOPIC)
         self.mqtt.start()
-
+    
     def on_message(self, client, userdata, msg):
         score = int(msg.payload.decode())
         self.scores["Player"] += score
 
         if self.scores["Player"] > self.highscore:
             self.highscore = self.scores["Player"]
-
-        print(f"Score: {self.scores['Player']} |Highscore: {self.highscore}")
+            
+        print(f"Score: {self.scores['Player']} | Highscore: {self.highscore}")
