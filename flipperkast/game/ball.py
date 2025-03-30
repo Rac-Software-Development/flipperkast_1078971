22a0c1ce9e4d14189e552b975662e2c47d1c02b7
1078971-ibrahim <1078971@hr.nl>
2025-03-30 15:32:20 +0200

    refactor : ball.py added pymunk

--- flipperkast/game/ball.py
+++ flipperkast/game/ball.py
@@ -1,20 +1,44 @@
+import pymunk
+import math
+
 class Ball:
-    def __init__(self, x: float, y: float):
-        self.x = x
-        self.y = y
-        self.velocity_x = 2
-        self.velocity_y = 3
-
-    def move(self):
-        self.velocity_y += 0.1
+    def __init__(self, space, position=(565, 650), radius=14):
+        mass = 1
+        inertia = pymunk.moment_for_circle(mass, 0, radius, (0, 0))
         
-        self.x += self.velocity_x
-        self.y += self.velocity_y
+        self.body = pymunk.Body(mass, inertia)
+        self.body.position = position
+        
+        self.body.angular_damping = 0.7
+        self.body.damping = 0.9
+        
+        self.shape = pymunk.Circle(self.body, radius)
+        self.shape.elasticity = 0.6
+        self.shape.friction = 0.7
+        
+        self.shape.collision_type = 1
+        
+        space.add(self.body, self.shape)
+        
+        self.setup_collision_handlers(space)
+        
+    def setup_collision_handlers(self, space):
+        wall_handler = space.add_collision_handler(1, 0)
+        wall_handler.pre_solve = self.handle_wall_collision
+        
+    def handle_wall_collision(self, arbiter, space, data):
+        normal = arbiter.normal
+        velocity = self.body.velocity
 
-        if self.x <= 10 or self.x >= 790:
-            self.velocity_x *= -1
-        if self.y <= 10:
-            self.velocity_y *= -1
-
-    def check_collision(self, obj):
-        pass
+        dot_product = velocity.dot(normal)
+        reflection = velocity - 2 * dot_product * normal
+        
+        damping_factor = 0.9
+        reflection = reflection * damping_factor
+        
+        self.body.velocity = reflection
+        
+        return True
+        
+    def apply_impulse(self, impulse):
+        self.body.apply_impulse_at_local_point(impulse)
